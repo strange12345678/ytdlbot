b6935f012d3010de3c7ee291f6a269f41abb7a86
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-06-11 16:18:34 +0530

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -29,7 +29,7 @@ WORKERS: int = get_env("WORKERS", 100)
 APP_ID: int = get_env("APP_ID")
 APP_HASH = get_env("APP_HASH")
 BOT_TOKEN = get_env("BOT_TOKEN")
-OWNER = [int(i) for i in str(get_env("OWNER")).split("5282274756")]
+OWNER = [int(i) for i in str(get_env("OWNER")).split(",")]
 # db settings
 AUTHORIZED_USER: str = get_env("AUTHORIZED_USER", "")
 DB_DSN = get_env("DB_DSN")
